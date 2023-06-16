dfdc5526da3d4e438d3a2d65f1c636f57939891b
Marko Kosyk <kosyk.markian@gmail.com>
2023-06-16 15:42:47 +0300

    Frontend init

---
+++ gamefrontend/src/Components/Mainpage/Mainpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mainpage() {
+  return (
+    <div className="text-3xl font-bold bold container mx-auto px-15 max-w-screen-sm text-center">
+      Welcome to the game
+    </div>
+  )
+}
+
+export default Mainpage
