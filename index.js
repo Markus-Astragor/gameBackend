0ca53271630bb8c3e9a78c27f59490ddd90fb741
Marko Kosyk <kosyk.markian@gmail.com>
2023-06-18 21:03:10 +0300

    frontend and backend

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const MongoDbURL = process.env.MONGO_DB_URL
 const mongoDbConnect = require('./setup/mongoose');
 const Register = require('./api/Register');
 const Login = require('./api/Login');
+const cors = require('cors');
 
 
 async function Setup() {
   app.use(express.json())
+  app.use(cors())
   await mongoDbConnect.Start(MongoDbURL)
   app.use(Register.router);
   app.use(Login.router);
